54b7586d9134409597245f2eb51c9d9bd7b83683
RahulSuroliya <suroliarahul9694@gmail.com>
2024-10-23 14:25:05 +0530

    done

---
+++ my_project-angular/src/app/purchaseorder-list/purchaseorder-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PurchaseorderListComponent } from './purchaseorder-list.component';
+
+describe('PurchaseorderListComponent', () => {
+  let component: PurchaseorderListComponent;
+  let fixture: ComponentFixture<PurchaseorderListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PurchaseorderListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PurchaseorderListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
